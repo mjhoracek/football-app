df3e1f297edd3e8133de7fe1bcc8d0bc2f7481a0
Mitch HoraceK <mitch@inthezone.dev>
2021-10-13 15:34:04 -0400

    FOUC fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,33 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 
 export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    //eslint-disable-next-line
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
     render() {
         return (
             <Html lang="en">
