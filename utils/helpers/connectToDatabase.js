299a8b3a812418a227899c8bb9c1c2947e36d191
Mitch HoraceK <mitch@inthezone.dev>
2021-10-13 12:44:46 -0400

    .

--- utils/helpers/connectToDatabase.js
+++ utils/helpers/connectToDatabase.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI =  `mongodb+srv://${process.env.NEXT_PUBLIC_DB_USER}:${process.env.NEXT_PUBLIC_DB_PASSWORD}@cluster0.90jzg.mongodb.net/MernPractice?retryWrites=true&w=majority`
+const MONGODB_URI = `mongodb+srv://${process.env.NEXT_PUBLIC_DB_USER}:${process.env.NEXT_PUBLIC_DB_PASSWORD}@cluster0.90jzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 if (!MONGODB_URI) {
     throw new Error(
