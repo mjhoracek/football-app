a306966d1d8e5fc654f46c114a1f6859e3477528
Mitch HoraceK <mitch@inthezone.dev>
2021-10-13 15:41:56 -0400

    .

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheets } from "@material-ui/core/styles";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
