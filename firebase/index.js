1635a3694a25873bc33a94f48fb6e2cec83964d9
Mitch HoraceK <mitch@inthezone.dev>
2021-10-13 11:36:34 -0400

    .

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    apiKey: 'AIzaSyAVQQnYy4kYbtvqyB2yE8oP7-cNf7ao6g8',
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
